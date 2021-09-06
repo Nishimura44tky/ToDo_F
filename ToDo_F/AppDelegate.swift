a32478f360e565f3912d4585bd420b5bce841802
Nishimura44tky <83286066+Nishimura44tky@users.noreply.github.com>
2021-09-06 11:12:59 +0900

    20210906

--- ToDo_F/AppDelegate.swift
+++ ToDo_F/AppDelegate.swift
@@ -7,6 +7,11 @@
 
 import UIKit
 
+// ↓ここと
+import Firebase
+// ↓Firebaseとは関係ないですがこちらも記述しておいてください
+import IQKeyboardManagerSwift
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,6 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // ↓ここに追記
+        FirebaseApp.configure()
+        // ↓Firebaseとは関係ないですがこちらも記述しておいてください
+        IQKeyboardManager.shared.enable = true
         return true
     }
 
---
+++ ToDo_F/TodoAddViewController.swift
@@ -0,0 +1,58 @@
+//
+//  TodoAddViewController.swift
+//  ToDo_F
+//
+//  Created by 西村拓也 on 2021/09/02.
+//
+
+import UIKit
+import Firebase
+
+class TodoAddViewController: UIViewController {
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var detailTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        // TextViewのレイアウトをTextField似合わせるためのコード
+        detailTextView.layer.borderWidth = 1.0
+        detailTextView.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0).cgColor
+        detailTextView.layer.cornerRadius = 5.0
+        detailTextView.layer.masksToBounds = true
+    }
+    
+    @IBAction func tapAddButton(_ sender: Any) {
+        if let title = titleTextField.text,
+            let detail = detailTextView.text {
+            // ②ログイン済みか確認
+            if let user = Auth.auth().currentUser {
+                // ③FirestoreにTodoデータを作成する
+        let createdTime = FieldValue.serverTimestamp()
+                Firestore.firestore().collection("users/\(user.uid)/todos").document().setData(
+                    [
+                     "title": title,
+                     "detail": detail,
+                     "isDone": false,
+                     "createdAt": createdTime,
+                     "updatedAt": createdTime
+                    ],merge: true
+                    ,completion: { error in
+                        if let error = error {
+                            // ③が失敗した場合
+                            print("TODO作成失敗: " + error.localizedDescription)
+                            let dialog = UIAlertController(title: "TODO作成失敗", message: error.localizedDescription, preferredStyle: .alert)
+                            dialog.addAction(UIAlertAction(title: "OK", style: .default))
+                            self.present(dialog, animated: true, completion: nil)
+                        } else {
+                            print("TODO作成成功")
+                            // ④Todo一覧画面に戻る
+                            self.dismiss(animated: true, completion: nil)
+                        }
+                })
+            }
+        }
+    }
+}
